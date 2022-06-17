70e3eac02d1fe82cbf005d76cf11ebe4f63ecef1
SiarheiPailak <vitiswood@gmail.com>
2022-06-17 14:13:32 +0300

    change JS

--- js/slider.js
+++ js/slider.js
@@ -2,8 +2,8 @@ let n = 1, max = 6;
 
     function changePic(){
         if(n > max) n = 1;
-        slider_jmg.src = `images/slider/${n++}s.jpg`;
-        setTimeout(changePic, 3000);
+        slider_jmg.src = `images/slider/${n++}s.JPG`;
+        setTimeout(changePic, 4000);
         };
 
 
